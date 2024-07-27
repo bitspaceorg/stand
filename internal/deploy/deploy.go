80a01866a6f8b3d702da16a29c334792b29934ea
Rahul M. Navneeth <50259284+RahulNavneeth@users.noreply.github.com>
2024-07-27 18:57:41 +0530

    Runtime (#3)
    * [Add] runtime installer
    
    * [Add] deploy package
    
    * [Fix] bug fixes runtime install 🪲
    
    * [Add] example for deployment ➕
    
    ---------
    
    Co-authored-by: suryaprakash <surya0prakash@proton.me>

---
+++ internal/deploy/deploy.go
@@ -0,0 +1,55 @@
+package deploy
+
+import (
+	"context"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+
+	parser "github.com/bitspaceorg/STAND/internal/build-parser"
+	"github.com/bitspaceorg/STAND/internal/runnable"
+	"github.com/bitspaceorg/STAND/internal/runtime"
+)
+
+func DeployGo(builPath string) {
+	var BuildConfig parser.NodeBuildConfig
+	parser := parser.NewBuildFileParser(builPath)
+	parser.Parse(&BuildConfig)
+	if BuildConfig.Requirements.Language != "node" {
+		log.Fatalf("Its not node!")
+	}
+	r := runtime.NodeRuntimeInstaller{
+		Home: BuildConfig.Project.Home, Version: BuildConfig.Requirements.Version,
+	}
+	err := r.Install()
+	if err != nil {
+		if !runtime.IsExitCode(3, err) {
+			log.Fatalf("[Error] :%v", err.Error())
+		} 
+	}
+
+	cmd := exec.Command("n", BuildConfig.Requirements.Version)
+	if err := cmd.Run(); err != nil {
+		log.Fatalf("Could not switch version")
+	}
+
+	for _, rawCmd := range BuildConfig.Build {
+		cmds := strings.Split(rawCmd.Cmd, " ")
+		buildCmd := exec.Command(cmds[0], cmds[1:]...)
+		buildCmd.Dir = BuildConfig.Project.Home
+		buildCmd.Stdout = os.Stdout
+		buildCmd.Stderr = os.Stderr
+		if err := buildCmd.Run(); err != nil {
+			log.Fatalf("Error in %v", rawCmd.Name)
+		}
+	}
+
+	cfg := runnable.NewStandConfig(BuildConfig.Project.Name, BuildConfig.Run[0].Cmd, BuildConfig.Project.Home, BuildConfig.Project.LogDir)
+
+	runner, err := runnable.NewStandRunner(context.Background(), cfg)
+	if err != nil {
+		log.Fatalf("Error:%v", err)
+	}
+	runner.Run()
+}
---
+++ internal/runtime/runtimeInstaller.go
@@ -0,0 +1,79 @@
+package runtime
+
+import (
+	"errors"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+// Each step in the install of the runtime
+type Installstep struct {
+	cmd []string
+	err string
+}
+
+type RuntimeInstaller interface {
+	// installs the runtime of the specific language to the local repository
+	Install()
+}
+
+type NodeRuntimeInstaller struct {
+	fetcher string
+	Home    string
+	Version string
+}
+
+// runs the command and throws the error
+// it will block until the command is completed with a exit code
+func (i *NodeRuntimeInstaller) runCommand(cmdString string) error {
+	cmds := strings.Split(cmdString, " ")
+	cmd := exec.Command(cmds[0], cmds[1:]...)
+	cmd.Dir = i.Home
+
+	cmd.Stdout = os.Stdout
+	cmd.Stdin = os.Stdin
+
+	err := cmd.Run()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// check for the installation of fetch tool
+func (i *NodeRuntimeInstaller) GetFetcher() error {
+	toRun := []string{"which curl", "which wget"}
+	for _, v := range toRun {
+		err := i.runCommand(v)
+		if err == nil {
+			i.fetcher = strings.Split(v, " ")[1]
+			return nil
+		}
+	}
+	return errors.New("Your majesty, wget or curl is required for this peasant!")
+}
+
+func (i *NodeRuntimeInstaller) Install() error {
+	var err error
+	err = i.GetFetcher()
+	if err != nil {
+		return err
+	}
+	err = i.runCommand(i.fetcher + " -L -o node_install.sh https://bit.ly/n-install")
+	if err != nil {
+		log.Printf("Fetcher error: %v", err.Error())
+		return errors.New("Could not access " + i.fetcher)
+	}
+
+	err = i.runCommand("chmod +x node_install.sh")
+	if err != nil {
+		return errors.New("could not give permission")
+	}
+	err = i.runCommand("bash node_install.sh -y")
+	if err != nil {
+		return err
+	}
+	return nil
+}
