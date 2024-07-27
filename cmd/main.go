20d3e9dff077c87dd94f9287588b65710615d29d
suryaprakash <surya0prakash@proton.me>
2024-07-27 18:45:57 +0530

    [Add] example for deployment ➕

--- cmd/main.go
+++ cmd/main.go
@@ -3,7 +3,6 @@ package main
 import "github.com/bitspaceorg/STAND/internal/deploy"
 
 func main() {
-	go deploy.DeployGo("./internal/build-parser/test.yml")
-	go deploy.DeployGo("./internal/build-parser/test2.yml")
+	go deploy.DeployGo("./example/test.yml")
 	select {}
 }
---
+++ example/example-project/index.js
@@ -0,0 +1,16 @@
+// Import the Express module
+const express = require('express');
+
+// Create a new Express application
+const app = express();
+
+// Define a route for the root URL
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// Start the server on port 3000
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
